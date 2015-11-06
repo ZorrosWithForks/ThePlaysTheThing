575ec288515223f1a7f115b82471582dcbab23cf
ArchimedesTeamLeader <lricha4296@students.pcci.edu>
2015-11-05 19:48:51 -0600

    Tweaked name generation for countries

--- MapClasses.py
+++ MapClasses.py
@@ -10,18 +10,16 @@ class unitCounts:
 
 class Country:
    def __init__(self):
-      self.name = random.choice("PKBWLTCMNDL")
+      self.name =  random.choice("PKBWLTCMNDL")
       self.name += random.choice("aeiou")
-      self.name += random.choice("pkbwltcmndl")
+      self.name += random.choice("pblmnd")
+      self.name += random.choice("pbwtcmnd")
       self.name += random.choice("aeiou")
-      self.name += random.choice("pkbwltcmndl")
-      self.name += random.choice(["ington", "ford", "stan", "burgh", "mark"])
-      print(self.name)
+      self.name += random.choice("pbwltcmndts")
+      self.name += random.choice(["ington", "ford", "stan", "burgh", "mark", "ania", "mouth"])
       self.owner = None
       self.attack_bonus = random.randint(1, 10)
       self.defense_bonus = random.randint(1, 15)
-      print(self.attack_bonus)
-      print(self.defense_bonus)
       self.unit_production = 1
       self.unit_counts = unitCounts(random.randint(1, 5), random.randint(0, 2), 0, 0)
       
