dbec10ea411d58db1da4875d2349c356db1f0c88
Caleb Mann <15soccer77@gmail.com>
2015-12-03 18:31:33 -0600

    added Nathan's stuff

--- game3/Player.py
+++ game3/Player.py
@@ -1,5 +1,24 @@
 class Player:
    def __init__(self, user_name, connection_object):
       self.user_name = user_name
-      self.unit_counts = 50 #This is subject to change
+      self.unit_points = 50 #This is subject to change
       self.connection_object = connection_object #Connection data object
+      self.unit_totals       = unitCounts(0, 0, 0, 0)
+      
+def addUnits(player, country, unit)
+   if unit == "infantry" && player.unit_points >= 3
+      player.unit_points -= 3
+      country.unitCounts.infantry += 1
+      player.unit_totals.infantry += 1
+   elif unit == "archer" && player.unit_points >= 5
+      player.unit_points -= 5
+      country.unitCounts.archers += 1
+      player.unit_totals.archers += 1
+   elif unit == "cannon" && player.unit_points >= 5
+      player.unit_points -= 5
+      country.unitCounts.cannons += 1
+      player.unit_totals.cannons += 1
+   elif unit == "champion" && player.unit_points >= 7
+      player.unit_points -= 7
+      country.unitCounts.champions += 1
+      player.unit_totals.champions += 1
