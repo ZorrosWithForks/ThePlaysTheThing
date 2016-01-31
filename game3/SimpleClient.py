99d59634dffa419ea787cc0a75cc730ff50ec197
ArchimedesTeamLeader <lricha4296@students.pcci.edu>
2016-01-30 18:33:35 -0600

    Made countries selectable.

--- game3/SimpleClient.py
+++ game3/SimpleClient.py
@@ -37,10 +37,13 @@ DEEP_WATER = 10
 OVERLAY = 9
 
 # Graphics Constants
+MOUSE_OVER = pygame.image.load(IMAGE_FILE_PATH + 'MouseOver.png')
+MOUSE_OVER_UNKNOWN = pygame.image.load(IMAGE_FILE_PATH + 'MouseOverUnknown.png')
 INFO_MARQUEE = pygame.image.load(IMAGE_FILE_PATH + "InfoMarque.png")
 INFO_OVERLAY = pygame.image.load(IMAGE_FILE_PATH + "InfoMarqueOverlay.png")
 MAP_FRAME = pygame.image.load(IMAGE_FILE_PATH + "MapFrame.png")
 MAP_LIGHT = pygame.image.load(IMAGE_FILE_PATH + "MapLighting.png")
+SELECTED_TILE = pygame.image.load(IMAGE_FILE_PATH + "Selected.png")
 
 #a dictionary linking resources to textures
 textures =   {
@@ -83,8 +86,65 @@ def moveMap(x_offset, y_offset, map):
       for column in range(map.WIDTH):
          ll_temp_map[row][column] = map.ll_map[(row + y_offset) % map.HEIGHT][(column + x_offset) % map.WIDTH]
    map.ll_map = ll_temp_map
-   
-def printMap(map, DISPLAYSURF):
+
+def standardInfo(map, DISPLAYSURF, params):
+    #Highlight country mouse is over and display country info
+    curr_x, curr_y = pygame.mouse.get_pos()
+    if (curr_x < map.WIDTH * TILESIZE and curr_y < map.HEIGHT * TILESIZE and map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)] != map.WATER):
+      curr_country = map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)]
+      DISPLAYSURF.blit(MOUSE_OVER if map.d_continents[curr_country[0]][curr_country[1]].owner != None else MOUSE_OVER_UNKNOWN, (int(curr_x / TILESIZE) * TILESIZE - MARGIN, int(curr_y / TILESIZE) * TILESIZE - MARGIN), special_flags=BLEND_ADD)
+      current_tile = map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)]
+      DISPLAYSURF.blit(CONTINENT_FONT.render("Continent: " + current_tile[0], True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 120))
+      DISPLAYSURF.blit(COUNTRY_FONT.render("Country: " + map.d_continents[current_tile[0]][current_tile[1]].name, True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 170))
+      
+      if map.d_continents[current_tile[0]][current_tile[1]].owner != None: 
+         # Unit counts by type
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Pistoleers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.infantry), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 225))
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Musketeers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.archers), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 250))
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Cannons: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.cannons), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 275))
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Airships: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.champions), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 300))
+         
+         # Country bonuses
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Attack Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].attack_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 350))
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Defense Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].defense_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 375))
+         
+         # Owner
+         DISPLAYSURF.blit(COUNTRY_FONT.render("Owner: " + str(map.d_continents[current_tile[0]][current_tile[1]].owner if map.d_continents[current_tile[0]][current_tile[1]].owner != None else "Neutral"), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 425))
+         
+         DISPLAYSURF.blit(CONTINENT_FONT.render("Continent Bonus: " + str(map.d_bonuses[current_tile[0]]), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 500))
+    else:
+      x_offset = 120
+      y_offset = 180
+      DISPLAYSURF.blit(CONTINENT_FONT.render("Players:", True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 130))
+      for name in map.l_player_names:
+         DISPLAYSURF.blit(COUNTRY_FONT.render(name, True, (0,0,0)), (map.WIDTH * TILESIZE + x_offset, y_offset))
+         y_offset += 25
+      y_offset += 25
+      DISPLAYSURF.blit(CONTINENT_FONT.render("Continent Strengths:", True, (0,0,0)), (map.WIDTH * TILESIZE + 100, y_offset))
+      y_offset += 20
+      for continent_name in map.l_continent_names:
+         y_offset += 25
+         DISPLAYSURF.blit(COUNTRY_FONT.render(continent_name + ": " + str(map.d_bonuses[continent_name]), True, (0,0,0)), (map.WIDTH * TILESIZE + x_offset, y_offset))
+
+def selectedInfo(map, DISPLAYSURF, params):
+    current_tile = map.ll_map[params[1]][params[0]]
+
+    # Unit counts by type
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Pistoleers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.infantry), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 225))
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Musketeers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.archers), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 250))
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Cannons: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.cannons), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 275))
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Airships: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.champions), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 300))
+    
+    # Country bonuses
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Attack Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].attack_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 350))
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Defense Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].defense_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 375))
+    
+    # Owner
+    DISPLAYSURF.blit(COUNTRY_FONT.render("Owner: " + str(map.d_continents[current_tile[0]][current_tile[1]].owner if map.d_continents[current_tile[0]][current_tile[1]].owner != None else "Neutral"), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 425))
+    
+    DISPLAYSURF.blit(CONTINENT_FONT.render("Continent Bonus: " + str(map.d_bonuses[current_tile[0]]), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 500))
+    
+def printMap(map, DISPLAYSURF, infoDisplay, params=None):
     #loop through each row
     for row in range(map.HEIGHT):
         #loop through each column in the row
@@ -127,48 +187,133 @@ def printMap(map, DISPLAYSURF):
     DISPLAYSURF.blit(source=INFO_MARQUEE, dest=(map.WIDTH * TILESIZE, 0))
     DISPLAYSURF.blit(source=pygame.image.load(IMAGE_FILE_PATH + "BaseBoard.png"), dest=(0, map.HEIGHT * TILESIZE))
     
-    #Highlight country mouse is over and display country info
-    curr_x, curr_y = pygame.mouse.get_pos()
-    if (curr_x < map.WIDTH * TILESIZE and curr_y < map.HEIGHT * TILESIZE and map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)] != map.WATER):
-      DISPLAYSURF.blit(pygame.image.load(IMAGE_FILE_PATH + 'MouseOver.png'), (int(curr_x / TILESIZE) * TILESIZE - MARGIN, int(curr_y / TILESIZE) * TILESIZE - MARGIN), special_flags=BLEND_ADD)
-      current_tile = map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)]
-      DISPLAYSURF.blit(CONTINENT_FONT.render("Continent: " + current_tile[0], True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 120))
-      DISPLAYSURF.blit(COUNTRY_FONT.render("Country: " + map.d_continents[current_tile[0]][current_tile[1]].name, True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 170))
-      
-      if map.d_continents[current_tile[0]][current_tile[1]].owner != None: 
-         # Unit counts by type
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Pistoleers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.infantry), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 225))
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Musketeers: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.archers), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 250))
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Cannons: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.cannons), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 275))
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Airships: " + str(map.d_continents[current_tile[0]][current_tile[1]].unit_counts.champions), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 300))
-         
-         # Country bonuses
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Attack Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].attack_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 350))
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Defense Bonus: " + str(map.d_continents[current_tile[0]][current_tile[1]].defense_bonus), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 375))
-         
-         # Owner
-         DISPLAYSURF.blit(COUNTRY_FONT.render("Owner: " + str(map.d_continents[current_tile[0]][current_tile[1]].owner if map.d_continents[current_tile[0]][current_tile[1]].owner != None else "Neutral"), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 425))
-         
-         DISPLAYSURF.blit(CONTINENT_FONT.render("Continent Bonus: " + str(map.d_bonuses[current_tile[0]]), True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 500))
-    else:
-      x_offset = 120
-      y_offset = 180
-      DISPLAYSURF.blit(CONTINENT_FONT.render("Players:", True, (0,0,0)), (map.WIDTH * TILESIZE + 100, 130))
-      for name in map.l_player_names:
-         DISPLAYSURF.blit(COUNTRY_FONT.render(name, True, (0,0,0)), (map.WIDTH * TILESIZE + x_offset, y_offset))
-         y_offset += 25
-      y_offset += 25
-      DISPLAYSURF.blit(CONTINENT_FONT.render("Continent Strengths:", True, (0,0,0)), (map.WIDTH * TILESIZE + 100, y_offset))
-      y_offset += 20
-      for continent_name in map.l_continent_names:
-         y_offset += 25
-         DISPLAYSURF.blit(COUNTRY_FONT.render(continent_name + ": " + str(map.d_bonuses[continent_name]), True, (0,0,0)), (map.WIDTH * TILESIZE + x_offset, y_offset))
+    infoDisplay(map, DISPLAYSURF, params)
 
     DISPLAYSURF.blit(source=INFO_OVERLAY, dest=(map.WIDTH * TILESIZE, 0), special_flags=BLEND_RGBA_ADD)
 
-    #update the display
-    pygame.display.update()
+def handleGeneral(event, map, map_X_offset, map_Y_offset):
+  if event.type == QUIT:
+      #and the game and close the window
+      pygame.quit()
+      sys.exit()
+  pygame.mouse.set_visible(True)
 
+  #if a key is pressed
+  if event.type == KEYDOWN:
+      if event.key == K_i:
+         pygame.display.iconify()
+      if event.key == K_ESCAPE:
+         #and the game and close the window
+         pygame.quit()
+         sys.exit()
+      #if the right arrow is pressed
+      if event.key == K_RIGHT:
+         #Change the map render offset
+         map_X_offset = (map_X_offset + 1) % map.WIDTH
+         moveMap(-1, 0, map)
+      if event.key == K_LEFT:
+         #Change the map render offset
+         map_X_offset = (map_X_offset - 1) % map.WIDTH
+         moveMap(1, 0, map)
+      if event.key == K_UP:
+         #Change the map render offset
+         map_Y_offset = (map_Y_offset - 1) % map.HEIGHT
+         moveMap(0, 1, map)
+      if event.key == K_DOWN:
+         #Change the map render offset
+         map_Y_offset = (map_Y_offset + 1) % map.HEIGHT
+         moveMap(0, -1, map)
+
+def placeUnits(DISPLAYSURF, map, map_X_offset, map_Y_offset):
+   BUY_PISTOLEERS = pygame.image.load(IMAGE_FILE_PATH + "PistoleersBuy.png")
+   BUY_MUSKETEERS = pygame.image.load(IMAGE_FILE_PATH + "MusketeersBuy.png")
+   BUY_CANNONS = pygame.image.load(IMAGE_FILE_PATH + "CannonBuy.png")
+   BUY_AIRSHIPS = pygame.image.load(IMAGE_FILE_PATH + "AirshipBuy.png")
+   placing = True
+   selectedCountry = None
+   
+   while placing:          
+       #get all the user events
+       for event in pygame.event.get():
+           #if the user wants to quit
+           if selectedCountry == None:
+              handleGeneral(event, map, map_X_offset, map_Y_offset)
+           
+           if event.type == MOUSEBUTTONDOWN:
+             curr_x, curr_y = pygame.mouse.get_pos()
+             curr_country = map.ll_map[int(curr_y / TILESIZE)][int(curr_x / TILESIZE)] if curr_x < map.WIDTH * TILESIZE and curr_y < map.HEIGHT * TILESIZE else map.WATER
+             if (curr_country != map.WATER and map.d_continents[curr_country[0]][curr_country[1]].owner == map.current_player):
+               if selectedCountry == None or selectedCountry != (int(curr_x / TILESIZE), int(curr_y / TILESIZE)):
+                  selectedCountry = (int(curr_x / TILESIZE), int(curr_y / TILESIZE))
+               else:
+                  selectedCountry = None
+                  
+       if selectedCountry != None:
+          selectedCountry = (selectedCountry[0] + map_X_offset, selectedCountry[1] + map_Y_offset)
+       
+       if selectedCountry == None:
+         printMap(map, DISPLAYSURF, standardInfo)
+       else:
+         printMap(map, DISPLAYSURF, selectedInfo, selectedCountry)
+       
+       if selectedCountry != None:
+         DISPLAYSURF.blit(SELECTED_TILE, (selectedCountry[0] * TILESIZE, selectedCountry[1] * TILESIZE), special_flags=BLEND_ADD)
+       
+       DISPLAYSURF.blit(BUY_PISTOLEERS, (60, map.HEIGHT * TILESIZE + 60))
+       DISPLAYSURF.blit(BUY_MUSKETEERS, (465, map.HEIGHT * TILESIZE + 60))
+       DISPLAYSURF.blit(BUY_CANNONS, (60, map.HEIGHT * TILESIZE + 115))
+       DISPLAYSURF.blit(BUY_AIRSHIPS, (465, map.HEIGHT * TILESIZE + 115))
+       
+       #update the display
+       pygame.display.update()
+       fpsClock.tick(10)
+       
+def declareAttacks(DISPLAYSURF, map, map_X_offset, map_Y_offset):
+   declaring = True
+
+   while declaring:
+       #get all the user events
+       for event in pygame.event.get():
+           #if the user wants to quit
+           handleGeneral(event, map, map_X_offset, map_Y_offset)
+                  
+       printMap(map, DISPLAYSURF)
+       
+       #update the display
+       pygame.display.update()
+       fpsClock.tick(10)
+       
+def resolveBattles(DISPLAYSURF, map, map_X_offset, map_Y_offset):
+   resolving = True
+
+   while resolving:
+       #get all the user events
+       for event in pygame.event.get():
+           #if the user wants to quit
+           handleGeneral(event, map, map_X_offset, map_Y_offset)
+                  
+       printMap(map, DISPLAYSURF)
+       
+       #update the display
+       pygame.display.update()
+       fpsClock.tick(10)
+       
+def moveTroops(DISPLAYSURF, map, map_X_offset, map_Y_offset):
+   moving = True
+
+   while moving:
+       #get all the user events
+       for event in pygame.event.get():
+           #if the user wants to quit
+           handleGeneral(event, map, map_X_offset, map_Y_offset)
+                  
+       printMap(map, DISPLAYSURF)
+       
+       #update the display
+       pygame.display.update()
+       fpsClock.tick(10)
+       
+         
 def play(host_address, player_name):
    map_X_offset = 0
    map_Y_offset = 0
@@ -202,80 +347,14 @@ def play(host_address, player_name):
    
    # Populate texture map index dictionary based on player names
    temp_index = 0
-   for player_name in map.l_player_names:
+   for name in map.l_player_names:
       temp_index += 1
-      d_playerLogoIndexes[player_name] = temp_index
+      d_playerLogoIndexes[name] = temp_index
+   
+   map.current_player = player_name
+   print(player_name)
    
    while True:
-       #get all the user events
-       for event in pygame.event.get():
-           #if the user wants to quit
-           if event.type == QUIT:
-               #and the game and close the window
-               pygame.quit()
-               sys.exit()
-           pygame.mouse.set_visible(True)
-
-           #if a key is pressed
-           if event.type == KEYDOWN:
-               if event.key == K_i:
-                  pygame.display.iconify()
-               if event.key == K_ESCAPE:
-                  #and the game and close the window
-                  pygame.quit()
-                  sys.exit()
-               #if the right arrow is pressed
-               if event.key == K_RIGHT:
-                  #Change the map render offset
-                  map_X_offset = (map_X_offset + 1) % map.WIDTH
-                  moveMap(-1, 0, map)
-               if event.key == K_LEFT:
-                  #Change the map render offset
-                  map_X_offset = (map_X_offset - 1) % map.WIDTH
-                  moveMap(1, 0, map)
-               if event.key == K_UP:
-                  #Change the map render offset
-                  map_Y_offset = (map_Y_offset - 1) % map.HEIGHT
-                  moveMap(0, 1, map)
-               if event.key == K_DOWN:
-                  #Change the map render offset
-                  map_Y_offset = (map_Y_offset + 1) % map.HEIGHT
-                  moveMap(0, -1, map)
-               
-               if event.key == K_m:
-                  #play the movie
-                  #screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-                  info = pygame.display.Info()
-                  movie_screen = pygame.Surface(movie.get_size())
-                  movie_screen.blit(textures[WATER], (200,200))
-                  movie.set_display(movie_screen)
-                  time_started = time.time()
-                  int(time_started)
-                  movie.set_volume(.99)
-                  movie.play()
-                  playing = True
-                  while playing:
-                     if event.type == KEYDOWN:
-                         if event.key == K_p:
-                            movie.pause()
-                         if event.key == K_s:
-                            movie.stop()
-                            playing = False
-                     current_time = time.time()
-                     int(current_time)
-                     print("current time is: ", str(current_time))
-                     DISPLAYSURF.blit(movie_screen,(map.WIDTH * TILESIZE + 100, 120))
-                     pygame.display.update()
-                     if current_time - time_started >= int(movie.get_length()):
-                        playing = False
-                        movie.rewind()
-                        pygame.event.clear()
-                        print("HERE?")
-                  print ("made it here")
-       printMap(map, DISPLAYSURF)
-       
-       #update the display
-       pygame.display.update()
-       fpsClock.tick(10)
+      placeUnits(DISPLAYSURF, map, map_X_offset, map_Y_offset)
 
    s.close()
